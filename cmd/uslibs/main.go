248d24d6b26efa847afc702688059da5a7c390d0
lemon-mint <ice.lemons.mint@gmail.com>
2022-10-16 08:49:09 +0900

    Tags

--- cmd/uslibs/main.go
+++ cmd/uslibs/main.go
@@ -60,6 +60,21 @@ func PureLink(u string) string {
 	return u
 }
 
+func FormatName(lib Library) string {
+	return fmt.Sprintf("%05d. %s", lib.ID, lib.Name)
+}
+
+var Links = map[string]string{}
+
+func GetLink(l string) string {
+	if v, ok := Links[l]; ok {
+		return v
+	}
+	al := GetAnchorLink(l)
+	Links[l] = al
+	return al
+}
+
 func main() {
 	var app App
 	_ = broccoli.BindOSArgs(&app)
@@ -90,15 +105,56 @@ func main() {
 			w := bufio.NewWriter(os.Stdout)
 			defer w.Flush()
 			w.WriteString("# Go Masterpieces\n\n")
+			GetLink("Go Masterpieces")
 			w.WriteString("Masterpieces of Go programming language.\n\n")
+
+			var allTagsMap = map[string][]Library{}
+			// Write Tags
+			w.WriteString("# Tags\n\n")
+			GetLink("Tags")
+			for _, lib := range libs {
+				for _, tag := range lib.Tags {
+					allTagsMap[tag] = append(allTagsMap[tag], lib)
+				}
+			}
+
+			var allTags []string
+			for tag := range allTagsMap {
+				allTags = append(allTags, tag)
+			}
+			sort.Strings(allTags)
+			for _, tag := range allTags {
+				w.WriteString(fmt.Sprintf("## %s\n\n", tag))
+				GetLink(tag)
+				for _, lib := range allTagsMap[tag] {
+					w.WriteString(fmt.Sprintf("* [%s](#%s)\n", FormatName(lib), GetLink(lib.URL)))
+				}
+				w.WriteString("\n\n")
+			}
+			w.WriteString("\n\n")
+
 			sort.Slice(libs, func(i, j int) bool {
 				return libs[i].ID < libs[j].ID
 			})
 			for _, lib := range libs {
-				w.WriteString(fmt.Sprintf("## %05d. %s\n\n", lib.ID, lib.Name))
+				w.WriteString(fmt.Sprintf("## %s\n\n", FormatName(lib)))
 				w.WriteString(fmt.Sprintf("URL: [%s](%s)\n\n", PureLink(lib.URL), lib.URL))
+				// Write Tags
+				if len(lib.Tags) > 0 {
+					w.WriteString("Tags: ")
+					for i, tag := range lib.Tags {
+						if i > 0 {
+							w.WriteString(", ")
+						}
+						w.WriteString(fmt.Sprintf("[%s](#%s)", tag, GetLink(tag)))
+					}
+					w.WriteString("\n\n")
+				}
 				w.WriteString(fmt.Sprintf("%s\n\n", lib.Description))
+
+				w.WriteString("\n\n")
 			}
+
 		}
 	case app.Del != nil:
 		err := DeleteLibrary(app.Del.ID)
